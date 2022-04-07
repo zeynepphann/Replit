73edc7bb327de6f7b7f43e8f700a3d12ef90919a
Zeynep HAN <zeynephan9906@gmail.com>
2022-04-07 22:44:16 +0300

    07.04.2022

---
+++ src/sorular/soru02.java
@@ -0,0 +1,23 @@
+package sorular;
+
+public class soru02 {
+    public static void main(String[] args) {
+        /*
+        Iki integer'i toplayan java programini yaziniz.
+
+int a=10
+
+int b=14
+
+Ornek Cikti:
+
+Sayilarin Toplami : 24
+         */
+
+        int a=10;
+        int b=14;
+
+        System.out.println(" iki sayinin toplami " + (a+b));
+
+    }
+}
---
+++ src/sorular/soru03.java
@@ -0,0 +1,24 @@
+package sorular;
+
+public class soru03 {
+    public static void main(String[] args) {
+        /*
+        Verilen integer sayilarin Toplamini, carpimini, cikarma islemini ve bolme islemini yapan java kodunu yaziniz.
+        num1=100;
+        num2=25;
+        Ornek Cikti:
+           125
+           2500
+           75
+           4
+         */
+        int num1=100;
+        int num2=25;
+        System.out.println("iki sayi toplami : "+ (num1+num2));
+        System.out.println("iki sayi carpimi : "+ (num1*num2));
+        System.out.println("cikartma islemi : "+ (num1-num2));
+        System.out.println("iki sayinin bolumu : "+ (num1/num2));
+
+
+    }
+}
